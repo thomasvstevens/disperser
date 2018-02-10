c6bed203b7d972a684f34b46a28284ba0613520c
thomasvstevens <thomasvstevens@gmail.com>
2018-02-10 12:33:23 -0800

    Test RandomLayout.

---
+++ RandomLayout.java
@@ -0,0 +1,28 @@
+package io.github.thomasvstevens.disperser;
+
+import java.util.Random;
+
+public class RandomLayout extends Layout {
+
+  private static final Random rand = new Random();
+
+  public RandomLayout(int m, int n) {
+    super(m, n);
+    permuteIds();
+  }
+
+  public void permuteIds() {
+    for (int k = m * n - 1; k > 0; k--) {
+      swapIds(k, rand.nextInt(k)); // 0 to k-1 inclusive
+    }
+  }
+
+  public static void main(String[] args) {
+    RandomLayout rlay = new RandomLayout(8, 12);
+    System.out.println(rlay);
+    RandomLayout rlayNew = new RandomLayout(8, 12);
+    System.out.println(rlayNew);
+    rlay.permuteIds();
+    System.out.println(rlay);
+  }
+}
